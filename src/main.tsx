44aa6ec0f4575da801abec29f09d9852f7dc40d0
dianamakarevych <129003607+dianamakarevych@users.noreply.github.com>
2026-05-29 09:14:02 +0200

    about

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,18 @@ import App from "./components/App/App";
 import MoodPage from "./pages/MoodPage";
 import History from "./pages/History";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import About from "./pages/About";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="827178068168-abmtijqsq3rc8v8gqok7vu2vs6h7u4ug.apps.googleusercontent.com">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MoodPage />} />
+          <Route path="/" element={<About />} />
           <Route path="/mood" element={<MoodPage />} />
           <Route path="/history/:teaName" element={<History />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </GoogleOAuthProvider>
